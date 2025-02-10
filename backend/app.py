dc94de525b1f6240989d426f09816c18a2b9a28c
deepakagrawalmsoe <138452263+deepakagrawalmsoe@users.noreply.github.com>
2025-02-11 00:54:19 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -18,4 +18,5 @@ def get_metrics():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Change from 8080 to 5000
+    app.run(host="0.0.0.0", port=port)
