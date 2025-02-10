bbfb1308e2e5951cd97af75d58fbcec03602fa37
deepakagrawalmsoe <138452263+deepakagrawalmsoe@users.noreply.github.com>
2025-02-11 00:16:45 +0530

    Create app.py

---
+++ backend/app.py
@@ -0,0 +1,21 @@
+import os
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+NAMESPACE = "deepakagrawalmsoe-dev"
+
+def fetch_metrics():
+    headers = {"Authorization": f"Bearer {os.getenv('TOKEN')}"}
+    url = f"https://api.sandbox-m2.openshift.com/apis/metrics.k8s.io/v1beta1/namespaces/{NAMESPACE}/pods"
+    response = requests.get(url, headers=headers, verify=False)
+    return response.json()
+
+@app.route('/metrics')
+def get_metrics():
+    data = fetch_metrics()
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
